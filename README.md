941fbd9f148e7ae0b31106b3c4838cff113517f3
Vasavi <vasavi.agarwal@gmail.com>
2020-07-15 09:29:28 +0530

    Refactoring of code and updates in README.md

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ You need to Install aws-php-sdk
 Here is a link which can be followed to do so - 
 https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/getting-started_installation.html
 
-You need to update your Bucket Name, IAM KEY, $IAM SECRET KEY, Directory to be Uploaded to Bucket, Destination for your Directory in S3 Bucket, Region of your S3 bucket
+You need to update your Bucket Name, IAM KEY, $IAM SECRET KEY, Directory to be Uploaded to Bucket, Destination for your Directory in S3 Bucket, Region of your S3 bucket in `UploadDirectory.php`
--- UploadDirectory.php
+++ UploadDirectory.php
@@ -11,19 +11,19 @@ $IAM_KEY = 'Your-IAM-Key'; //Enter Your IAM key here
 $IAM_SECRET = 'Your-IAM-Secret-Key'; //Enter Your IAM Secret Key
 $source = 'Directory-To-Upload'; //Enter the Directory you want to Upload
 $dest = 'Enter-The-Path-You-Want-To-Upload-The-Directory-To-In-Bucket'; //Eg. - s3://Your-Bucket-Name/Path/to/folder/In/S3/Bucket
-$region = 'Enter-the-region-of-your-S3-Bucket' //Eg. - 'us-east-2'	
+$region = 'Enter-the-region-of-your-S3-Bucket'; //Eg. - 'us-east-2'	
 
-try {	
-$s3 = S3Client::factory(
-	array(
-		'credentials' => array(
-			'key' => $IAM_KEY,
-			'secret' => $IAM_SECRET
-		),
-		'version' => 'latest',
-		'region'  => $region
-	)
-);
+try {
+	$s3 = S3Client::factory(
+		array(
+			'credentials' => array(
+				'key' => $IAM_KEY,
+				'secret' => $IAM_SECRET
+			),
+			'version' => 'latest',
+			'region'  => $region
+		)
+	);
 } 
 catch (Exception $e) 
 {
@@ -32,20 +32,16 @@ catch (Exception $e)
 
 
 $options[] = [
-'DEBUG' => true,
+	'DEBUG' => true,
 ];
 
 $manager = (new Transfer($s3, $source, $dest , $options));
 
 $promise = $manager->promise();
 $promise->then(function () {
-    echo 'Done!';
+	echo 'Done!';
 });
-
 
 $manager->transfer();
 
-
 ?>
-
-
